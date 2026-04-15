2f0a5d5e3335609d130a4cfc0dcf49cbe09a3de0
Yasmin_mascarenhas <yasmin.mascarenhas@escola.pr.gov.br>
2026-04-15 16:52:26 +0000

    Alterações Home Page

---
+++ app/src/components/lista.jsx
@@ -0,0 +1,16 @@
+export default function Lista() {
+    return (
+        <section>
+            <div>
+       <h2>Nosso Cardápio</h2>
+      {/* ul marcador ol numeral */}
+      <ul>
+        <li>Expresso</li>
+        <li>Cappucino</li>
+        <li>Latte</li>
+        <li>Mocha</li>
+      </ul>
+      </div>
+        </section>
+    );
+}
---
+++ app/src/components/texto.jsx
@@ -0,0 +1,20 @@
+export default function Texto() {
+    return (
+        <section>
+      <div>
+      {/* h1 ate h6 */}
+      <h1>Bem Vindo ao Café Aroma Grãos</h1>
+      {/* parágrafo */}
+      <p>o melhor café da cidade, feito com <strong>grãos</strong> selecionados e paixão pela arte de preparar café🍵</p>
+      <div>
+      <h1>Títulos</h1>
+      <h2>Subtitulos</h2>
+      <h3>Seção</h3>
+      <h4>Subseção</h4>
+      <h5>Detalhes</h5>
+      <h6>Menor titulo</h6>
+      </div>
+      </div>
+        </section>
+    );
+}
